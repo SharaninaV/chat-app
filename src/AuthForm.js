2a1fb2b7624741a6c35a204c127a3b6d9028c535
starlight3307 <viktoriya.sharanina@gmail.com>
2021-05-24 17:46:28 +0300

    Installed Formik. Integrated useFormik into AuthForm component.

--- src/AuthForm.js
+++ src/AuthForm.js
@@ -1,14 +1,26 @@
 import React from "react";
+import { useFormik } from "formik";
 
 const AuthForm = () => {
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values,null,2));
+        },
+    });
 
     return(
-        <form>
+        <form onSubmit={formik.handleSubmit}>
             <label htmlFor="email">Email</label>
             <input
                 id="email"
                 name="email"
                 type="email"
+                onChange={formik.handleChange}
+                value={formik.values.email}
                 placeholder="Email"
             />
             <label htmlFor="password">Password</label>
@@ -16,10 +28,12 @@ const AuthForm = () => {
                 id="password"
                 name="password"
                 type="password"
+                onChange={formik.handleChange}
+                value={formik.values.password}
             />
             <button type="submit">Submit</button>
         </form>
     );
-}
+};
 
 export { AuthForm };
