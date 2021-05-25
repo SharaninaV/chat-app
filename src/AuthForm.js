84b72ca00b96f163e00cfcc9d166b6688d3e9050
starlight3307 <viktoriya.sharanina@gmail.com>
2021-05-25 16:39:56 +0300

    Installed Yup. Added validation for email and password

--- src/AuthForm.js
+++ src/AuthForm.js
@@ -1,39 +1,67 @@
 import React from "react";
-import { useFormik } from "formik";
+import {useFormik} from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object({
+    email: Yup.string()
+        .email("Email должен иметь общепринятый вид адреса электронной почты.")
+        .required('Email должен быть введен.'),
+    password: Yup.string().required('Пароль должен быть введен.'),
+})
+
+const initialValues = {
+    email: '',
+    password: '',
+}
+
 
 const AuthForm = () => {
     const formik = useFormik({
-        initialValues: {
-            email: '',
-            password: '',
+        initialValues,
+        validationSchema: validationSchema,
+
+        onSubmit(values) {
+                alert(JSON.stringify(values, null, 2));
         },
-        onSubmit: values => {
-            alert(JSON.stringify(values,null,2));
-        },
+
     });
 
-    return(
+    return (
         <form onSubmit={formik.handleSubmit}>
-            <label htmlFor="email">Email</label>
-            <input
-                id="email"
-                name="email"
-                type="email"
-                onChange={formik.handleChange}
-                value={formik.values.email}
-                placeholder="Email"
-            />
-            <label htmlFor="password">Password</label>
-            <input
-                id="password"
-                name="password"
-                type="password"
-                onChange={formik.handleChange}
-                value={formik.values.password}
-            />
+            <div>
+                <label htmlFor="email">Email</label>
+                <input
+                    id="email"
+                    name="email"
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                    placeholder="Email"
+                />
+                {formik.touched.email && formik.errors.email ? (
+                    <div>
+                        {formik.errors.email}
+                    </div>) : null
+                }
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                />
+                {formik.touched.password && formik.errors.password ? (
+                    <div>
+                        {formik.errors.password}
+                    </div>) : null
+                }
+            </div>
             <button type="submit">Submit</button>
         </form>
     );
 };
 
-export { AuthForm };
+export {AuthForm};
