78258da08af8cb832e54cb9ffdd31471f45e0970
starlight3307 <viktoriya.sharanina@gmail.com>
2021-06-01 16:55:20 +0300

    Installed fontawesome. Added icons to email and password labels and submit button. Changed styles: vertical alignment of button and links

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -3,6 +3,12 @@ import {useFormik} from "formik";
 import * as Yup from "yup";
 import {connect} from "react-redux";
 import {loginRequest} from "../sagas/actionCreator";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faSignInAlt, faEnvelope, faKey} from '@fortawesome/free-solid-svg-icons'
+
+const iconSignIn = <FontAwesomeIcon icon={faSignInAlt}/>
+const iconEmail = < FontAwesomeIcon icon={faEnvelope}/>
+const iconPassword = < FontAwesomeIcon icon={faKey}/>
 
 const validationSchema = Yup.object({
     email: Yup.string()
@@ -33,22 +39,26 @@ const AuthForm = (props) => {
             {props.submitErrors.message ? (
                 <div className="row">
                     <div className="alert-danger col-sm-11">
-                    {props.submitErrors.message}
+                        {props.submitErrors.message}
                     </div>
                 </div>
             ) : null}
             <div className="input form-group row">
-                <label htmlFor="email" className="col-sm-2 col-form-label">Email</label>
-                <div className="col-sm-12">
-                <input
-                    id="email"
-                    name="email"
-                    type="text"
-                    onChange={formik.handleChange}
-                    value={formik.values.email}
-                    placeholder="Email"
-                    class="form-control"
-                />
+                <label htmlFor="email" className="col-sm-2 col-form-label d-flex">Email:
+                </label>
+                <div className="col-sm-10 d-flex">
+                    <div className="me-3">
+                        <i className="align-middle">{iconEmail}</i>
+                    </div>
+                    <input
+                        id="email"
+                        name="email"
+                        type="text"
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                        placeholder="Email"
+                        class="form-control"
+                    />
                 </div>
                 {formik.touched.email && formik.errors.email ? (
                     <div className="alert-danger">
@@ -57,16 +67,20 @@ const AuthForm = (props) => {
                 }
             </div>
             <div className="input form-group row">
-                <label htmlFor="password" className="col-sm-2 col-form-label">Пароль</label>
-                <div className="col-sm-12">
-                <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    onChange={formik.handleChange}
-                    value={formik.values.password}
-                    class="form-control"
-                />
+                <label htmlFor="password" className="col-sm-2 col-form-label">Пароль:
+                </label>
+                <div className="col-sm-10 d-flex">
+                    <div className="me-3">
+                        <i className="align-middle">{iconPassword}</i>
+                    </div>
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                        class="form-control"
+                    />
                 </div>
                 {formik.touched.password && formik.errors.password ? (
                     <div className="alert-danger">
@@ -75,7 +89,10 @@ const AuthForm = (props) => {
                 }
             </div>
             <div className="btn-submit">
-            <button type="submit" className="btn btn-primary">Войти</button>
+                <button type="submit" className="btn btn-primary">
+                    Войти
+                    <i className="ms-2">{iconSignIn}</i>
+                </button>
             </div>
         </form>
     );
@@ -91,6 +108,6 @@ const mapStateToProps = state => {
     }
 }
 
-const mapDispatchToProps = { loginRequest }
+const mapDispatchToProps = {loginRequest}
 
 export default connect(mapStateToProps, mapDispatchToProps)(AuthForm);
--- src/components/AuthWindow.js
+++ src/components/AuthWindow.js
@@ -8,10 +8,20 @@ const AuthWindow = () => {
         <div className="app container">
             <h1>ChatApp</h1>
             <AuthForm/>
-            <a href="#" className="passw-link">Забыли пароль?</a>
-            <a href="#" className="reg-link">Регистрация</a>
+            <div className="row justify-content-center">
+                <div className="col-sm-6">
+                    <a href="#" className="passw-link">
+                        <p className="text-end">Забыли пароль?</p>
+                    </a>
+                </div>
+                <div className="col-sm-6 reg-link">
+                    <a href="#" className="">
+                        <p>Регистрация</p>
+                    </a>
+                </div>
+            </div>
         </div>
-    )
+)
 }
 
 export {AuthWindow}
