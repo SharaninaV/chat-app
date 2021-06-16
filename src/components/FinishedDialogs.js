ee8874f6540fbae06e4b49de4fce530899201c8a
starlight3307 <viktoriya.sharanina@gmail.com>
2021-06-16 13:23:25 +0300

    Installed react-pretty-rating. Added finished dialogs view with rating

--- src/components/FinishedDialogs.js
+++ src/components/FinishedDialogs.js
@@ -1,9 +1,56 @@
 import React from "react";
+import {useSelector} from "react-redux";
+import {Col, Container, ListGroup, Row} from "react-bootstrap";
+import moment from "moment";
+import {faStar} from "@fortawesome/free-solid-svg-icons";
+import {faStar as farStar} from "@fortawesome/free-regular-svg-icons"
+import PrettyRating from "pretty-rating-react";
 
 const FinishedDialogs = () => {
 
-    return(
-        <h5>finished</h5>
+    const operatorID = useSelector((state) => state.auth.email)
+        .split('@')[0]
+    const finishedDialogs = useSelector((state) => state.fetchDialogs.fetchedDialogs)
+        .filter(dialog => dialog.data.status === 'finished' && dialog.data.operatorID === operatorID)
+
+    const icons = {
+        star: {
+            complete: faStar,
+            empty: farStar
+        }
+    }
+    const colors = {
+        star: ['#d9ad26', '#434b4d']
+    }
+
+
+
+    return (
+        <ListGroup>
+            {finishedDialogs.length > 0 ?
+                (finishedDialogs.map(dialog => (
+                        <ListGroup.Item action>
+                            <Container>
+                                <Row>
+                                    <Col>
+                                        {dialog.key}
+                                    </Col>
+                                    <Col>
+                                        ({moment(dialog.data.latestActivity).locale('ru').format('LLL')})
+                                    </Col>
+                                    <Col>
+                                        <PrettyRating value={dialog.data.rating} icons={icons.star} colors={colors.star}/>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </ListGroup.Item>
+                    ))
+                ) : (
+                    <ListGroup.Item>
+                        Диалогов не найдено
+                    </ListGroup.Item>
+                )}
+        </ListGroup>
     )
 }
 
