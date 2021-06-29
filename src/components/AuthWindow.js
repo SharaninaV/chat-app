d6a0e15375ad8aa4c49fb26d57558e589f1342f6
starlight3307 <viktoriya.sharanina@gmail.com>
2021-06-29 20:32:04 +0300

    Fixed layout of AuthWindow component

--- src/components/AuthWindow.js
+++ src/components/AuthWindow.js
@@ -2,35 +2,36 @@ import React, {useEffect} from "react";
 import {useSelector} from "react-redux";
 import {useHistory} from "react-router-dom"
 import AuthForm from "./AuthForm";
+import {Col, Container, Row} from "react-bootstrap";
 
 const AuthWindow = () => {
 
     const isTokenValid = useSelector((state) => state.main.isTokenValid)
     const history = useHistory()
     useEffect(() => {
-        if(isTokenValid) {
+        if (isTokenValid) {
             history.push("/main")
         }
-    },[isTokenValid])
+    }, [isTokenValid])
 
     return (
-        <div className="app container">
+        <Container className="app">
             <h1>ChatApp</h1>
             <AuthForm/>
-            <div className="row justify-content-center">
-                <div className="col-sm-6">
-                    <a href="#" className="passw-link">
+            <Row className="justify-content-center">
+                <Col md={3}>
+                    <a href="#">
                         <p className="text-end">Забыли пароль?</p>
                     </a>
-                </div>
-                <div className="col-sm-6 reg-link">
-                    <a href="#" className="">
+                </Col>
+                <Col md={3}>
+                    <a href="#">
                         <p>Регистрация</p>
                     </a>
-                </div>
-            </div>
-        </div>
-)
+                </Col>
+            </Row>
+        </Container>
+    )
 }
 
 export {AuthWindow}
