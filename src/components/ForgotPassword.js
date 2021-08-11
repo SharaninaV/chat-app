bbce4f68379b263efac61f216fbc593ab51a53d4
starlight3307 <viktoriya.sharanina@gmail.com>
2021-08-11 17:35:37 +0300

    Added links, styles DEVSH-127

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -2,7 +2,7 @@ import React from "react";
 import * as Yup from "yup";
 import {useFormik} from "formik";
 import {useDispatch} from "react-redux";
-import {Alert, Button, Col, Row} from "react-bootstrap";
+import {Alert, Button, Col, Container, Row} from "react-bootstrap";
 import {useHistory} from "react-router-dom";
 import {sendResetEmailRequest} from "../redux/forgotPassword/actionCreator";
 
@@ -33,35 +33,49 @@ export const ForgotPassword = () => {
     });
 
     return (
-        <form onSubmit={formik.handleSubmit} className="registerForm">
-            <h1>ChatApp</h1>
-            <h2>Восстановление пароля</h2>
-            {formik.touched.email && formik.errors.email &&
-            <Row>
-                <Col>
-                    <Alert variant="danger">
-                        {formik.errors.email}
-                    </Alert>
-                </Col>
-            </Row>
-            }
-            <Row className="form-input">
-                <Col md={2}>
-                    <label htmlFor="email" className="align-middle">Email:</label>
-                </Col>
-                <Col md={10}>
-                    <input
-                        id="email"
-                        name="email"
-                        type="text"
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
-                        placeholder="Email"
-                        className="form-control"
-                    />
-                </Col>
-            </Row>
-            <Button className="form-btn" type="submit">Отправить ссылку для восстановления</Button>
-        </form>
+        <Container className="forgotPassword">
+            <form onSubmit={formik.handleSubmit} className="registerForm">
+                <h1>ChatApp</h1>
+                <h2>Восстановление пароля</h2>
+                {formik.touched.email && formik.errors.email &&
+                <Row>
+                    <Col>
+                        <Alert variant="danger">
+                            {formik.errors.email}
+                        </Alert>
+                    </Col>
+                </Row>
+                }
+                <Row className="form-input">
+                    <Col md={2}>
+                        <label htmlFor="email" className="align-middle">Email:</label>
+                    </Col>
+                    <Col md={10}>
+                        <input
+                            id="email"
+                            name="email"
+                            type="text"
+                            onChange={formik.handleChange}
+                            value={formik.values.email}
+                            placeholder="Email"
+                            className="form-control"
+                        />
+                    </Col>
+                </Row>
+                <Button className="form-button" type="submit">Отправить ссылку для восстановления</Button>
+                <Row className="justify-content-center">
+                    <Col md={5}>
+                        <a href="/">
+                            <p>Войти</p>
+                        </a>
+                    </Col>
+                    <Col md={5}>
+                        <a href="/registration">
+                            <p>Регистрация</p>
+                        </a>
+                    </Col>
+                </Row>
+            </form>
+        </Container>
     )
 }
